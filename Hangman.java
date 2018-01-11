2d3c45f64bf00243fb161e390601ad5546c6b55d
SEAN MCLIMANS <seanmclimans@gmail.com>
2018-01-11 10:10:15 -0700

    prompt user for input

--- Hangman.java
+++ Hangman.java
@@ -3,5 +3,12 @@ public class Hangman {
   public static void main(String[] args) {
 
     Game game = new Game("television");
+    Prompter prompter = new Prompter(game);
+    boolean isHit = prompter.promptForGuess();
+    if (isHit) {
+      System.out.println("We got a hit!");
+    } else {
+      System.out.println("Oops missed");
+    }
   }
 }
